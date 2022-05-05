7ed223123d6863734f2bd18732c2ad770b71621b
saisarvani.ballamudi <saisarvani.ballamudi@tadigital.com>
2022-05-05 16:46:07 +0530

    Terraform

--- packages/b2b-terraform/default/backend.tf
+++ packages/b2b-terraform/default/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    credentials = "../../../keys/terraform-sa-fluted-volt-346608.json"
+    credentials = "../../../keys/terraform-sa-ct-b2b-348510.json"
     bucket      = "terraform-fluted-volt-346608"
     prefix      = "commercetools"
   }
